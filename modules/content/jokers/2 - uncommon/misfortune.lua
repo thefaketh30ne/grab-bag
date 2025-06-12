c7119bc5b9815066f764c8bc63d4ca977f37970f
BepisFever <nguyendoangiabao220808@gmail.com>
2025-06-12 20:24:25 +0700

    Update misfortune.lua

--- modules/content/jokers/2 - uncommon/misfortune.lua
+++ modules/content/jokers/2 - uncommon/misfortune.lua
@@ -25,8 +25,8 @@ SMODS.Joker {
 		end
 		if context.final_scoring_step then
 			if pseudorandom('gb_misfortune') < G.GAME.probabilities.normal / card.ability.extra.odds then		
-				hand_chips = 0
-				mult = 0
+				hand_chips = to_big(0)
+				mult = to_big(0)
 				return {
 					message = localize('k_nope_ex'),
 					colour = G.C.SECONDARY_SET.Tarot,
