7b873719d0bdc5541191756a9cf34f2983418605
thefaketh30ne <thefaketh30ne@gmail.com>
2025-06-12 21:05:16 +0100

    Custom music can now be turned off

--- modules/init/music_selector.lua
+++ modules/init/music_selector.lua
@@ -1,12 +1,16 @@
 music_pitch = 1
 
+if not gb_config.custom_music then
+    return
+end
+
 SMODS.Sound({
     vol = 0.45,
     pitch = music_pitch,
     key = "gb_music1",
     path = "gb_music1.ogg",
     select_music_track = function()
-        return (gb_config.custom_music) and 10 or false
+        return (gb_config.custom_music) or false
     end,
 })
 
@@ -16,7 +20,7 @@ SMODS.Sound({
     key = "gb_music2",
     path = "gb_music2.ogg",
     select_music_track = function()
-        return (gb_config.custom_music and G.booster_pack_sparkles and not G.booster_pack_sparkles.REMOVED) and 11 or false
+        return (gb_config.custom_music and G.booster_pack_sparkles and not G.booster_pack_sparkles.REMOVED) or false
     end,
 })
 
@@ -26,7 +30,7 @@ SMODS.Sound({
     key = "gb_music3",
     path = "gb_music3.ogg",
     select_music_track = function()
-        return (gb_config.custom_music and G.booster_pack_meteors and not G.booster_pack_meteors.REMOVED) and 11 or false
+        return (gb_config.custom_music and G.booster_pack_meteors and not G.booster_pack_meteors.REMOVED) or false
     end,
 })
 
@@ -36,7 +40,7 @@ SMODS.Sound({
     key = "gb_music4",
     path = "gb_music4.ogg",
     select_music_track = function()
-        return (gb_config.custom_music and G.shop and not G.shop.REMOVED) and 11 or false
+        return (gb_config.custom_music and G.shop and not G.shop.REMOVED)or false
     end,
 })
 
@@ -46,7 +50,7 @@ SMODS.Sound({
     key = "gb_music5",
     path = "gb_music5.ogg",
     select_music_track = function()
-        return (gb_config.custom_music and G.GAME.blind and G.GAME.blind.boss) and 11 or false
+        return (gb_config.custom_music and G.GAME.blind and G.GAME.blind.boss) or false
     end,
 })
 
