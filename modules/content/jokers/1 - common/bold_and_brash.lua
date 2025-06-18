b678f728a4cfc0d0bb8f845cd43d6fba135cdd33
thefaketh30ne <thefaketh30ne@gmail.com>
2025-06-18 21:52:00 +0100

    bugfixes

--- modules/content/jokers/1 - common/bold_and_brash.lua
+++ modules/content/jokers/1 - common/bold_and_brash.lua
@@ -21,11 +21,11 @@ SMODS.Joker {
             local _, _, poker_hands = G.FUNCS.get_poker_hand_info(G.hand.highlighted)
 			if next(poker_hands[card.ability.extra.hand_to_discard]) then
                 ease_hands_played(card.ability.extra.hands)
+                return {
+                    message = "+1 Hand",
+                    colour = G.C.BLUE
+                }
             end
-            return {
-                message = "+1 Hand",
-                colour = G.C.BLUE
-            }
 		end
     end
 }
