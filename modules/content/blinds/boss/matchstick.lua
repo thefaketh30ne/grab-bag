6269cc0402e8fba7d0b537e9f811ae8b9dece7c8
thefaketh30ne <thefaketh30ne@gmail.com>
2025-06-21 00:57:24 +0100

    matchstick fix

--- modules/content/blinds/boss/matchstick.lua
+++ modules/content/blinds/boss/matchstick.lua
@@ -14,17 +14,19 @@ SMODS.Blind {
     boss = { min = 3 },
     boss_colour = HEX("85675d"),
     calculate = function(self, blind, context)
+        if context.setting_blind then
+            blind.discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
+            blind.hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
+        end
         if not blind.disabled then
             if context.setting_blind then
-                blind.discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
-                blind.hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
                 ease_hands_played(-blind.discards_sub)
                 ease_discard(-blind.hands_sub)
             end
         end
     end,
     disable = function(self)
-        ease_discard(G.GAME.blind.discards_sub)
-        ease_hands_played(G.GAME.blind.hands_sub)
+        ease_discard(blind.discards_sub)
+        ease_hands_played(blind.hands_sub)
     end
 }
