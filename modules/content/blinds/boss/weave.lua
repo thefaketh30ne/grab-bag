89f1c1d5a3947d68d36718763d06281220f5705c
thefaketh30ne <thefaketh30ne@gmail.com>
2025-06-21 00:21:49 +0100

    talisman crash fixed

--- modules/content/blinds/boss/weave.lua
+++ modules/content/blinds/boss/weave.lua
@@ -20,7 +20,7 @@ SMODS.Blind {
                 blind.triggered = false
                 if #G.play.cards > 3 then
                     blind.triggered = true
-                    local half_dollars = math.floor(G.GAME.dollars / 2)
+                    local half_dollars = math.floor(to_number(to_big(G.GAME.dollars)) / 2)
                     if half_dollars > 0 then
                         ease_dollars(-half_dollars, true)
                     end
