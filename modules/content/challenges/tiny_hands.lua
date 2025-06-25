903ff1058023e3a920168a672f447dcc52b3a611
thefaketh30ne <thefaketh30ne@gmail.com>
2025-06-25 17:09:04 +0100

    new challenges and bugfixes

---
+++ modules/content/challenges/tiny_hands.lua
@@ -0,0 +1,34 @@
+SMODS.Challenge {
+    key = 'tiny_hands',
+    loc_txt = {
+        name = "Tiny Hands"
+    },
+    modifiers = {
+            { id = 'discards', value = 0 },
+            { id = 'hands', value = 2 },
+        },
+    deck = {
+        cards = {
+            { s = 'C', r = '5' },
+            { s = 'D', r = '5' },
+            { s = 'H', r = '5' },
+            { s = 'S', r = '5' },
+            { s = 'C', r = '4' },
+            { s = 'D', r = '4' },
+            { s = 'H', r = '4' },
+            { s = 'S', r = '4' },
+            { s = 'C', r = '3' },
+            { s = 'D', r = '3' },
+            { s = 'H', r = '3' },
+            { s = 'S', r = '3' },
+            { s = 'C', r = '2' },
+            { s = 'D', r = '2' },
+            { s = 'H', r = '2' },
+            { s = 'S', r = '2' },
+            { s = 'C', r = 'A' },
+            { s = 'D', r = 'A' },
+            { s = 'H', r = 'A' },
+            { s = 'S', r = 'A' },
+        }
+    }
+}
--- modules/content/consumables/ephemeral/portal.lua
+++ modules/content/consumables/ephemeral/portal.lua
@@ -22,7 +22,7 @@ SMODS.Consumable {
                 suit = suit,
                 edition = "e_gb_temporary",
                 rank = pseudorandom_element(
-                    {"2", "3", "4", "5", "6", "7", "8", "9", "10"},
+                    {"2", "3", "4", "5", "6", "7", "8", "9", "T"},
                     pseudoseed("gb_portal")
                 ),
                 area = G.discard
