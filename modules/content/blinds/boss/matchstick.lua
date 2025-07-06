05b80529c346d7f56a710a8f6ff953b480c6b844
thefaketh30ne <thefaketh30ne@gmail.com>
2025-07-06 01:50:28 +0100

    fixed matchstick disabling crash

--- modules/content/blinds/boss/matchstick.lua
+++ modules/content/blinds/boss/matchstick.lua
@@ -15,18 +15,22 @@ SMODS.Blind {
     boss_colour = HEX("85675d"),
     calculate = function(self, blind, context)
         if context.setting_blind then
-            blind.discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
-            blind.hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
+            blind.config.gb_discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
+            blind.config.gb_hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
         end
         if not blind.disabled then
             if context.setting_blind then
-                ease_hands_played(-blind.discards_sub)
-                ease_discard(-blind.hands_sub)
+                ease_hands_played(-blind.config.gb_discards_sub)
+                ease_discard(-blind.gb_hands_sub)
             end
         end
     end,
     disable = function(self)
-        ease_discard(blind.discards_sub)
-        ease_hands_played(blind.hands_sub)
+        if not blind.config.gb_discards_sub and blind.config.gb_hands_sub then
+            blind.config.gb_discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
+            blind.config.gb_hands_sub = math.floor(G.GAME.current_round.hands_left / 2) 
+        end
+        ease_discard(blind.config.gb_discards_sub)
+        ease_hands_played(blind.config.gb_hands_sub)
     end
 }
