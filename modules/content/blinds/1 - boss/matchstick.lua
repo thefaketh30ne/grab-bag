e2c788db5cd625a3ab30135679262c8a52950d84
thefaketh30ne <thefaketh30ne@gmail.com>
2025-07-08 18:45:47 +0100

    Update matchstick.lua

--- modules/content/blinds/1 - boss/matchstick.lua
+++ modules/content/blinds/1 - boss/matchstick.lua
@@ -13,24 +13,25 @@ SMODS.Blind {
     pos = { y = 13 },
     boss = { min = 3 },
     boss_colour = HEX("85675d"),
+    config = { extra = { discards_sub = 0, hands_sub = 0 } },
     calculate = function(self, blind, context)
         if context.setting_blind then
-            self.config.gb_discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
-            self.config.gb_hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
+            self.config.extra.discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
+            self.config.extra.hands_sub = math.floor(G.GAME.current_round.hands_left / 2)
         end
         if not blind.disabled then
             if context.setting_blind then
-                ease_hands_played(-self.config.gb_discards_sub)
-                ease_discard(-self.gb_hands_sub)
+                ease_hands_played(-self.config.extra.discards_sub)
+                ease_discard(-self.config.extra.hands_sub)
             end
         end
     end,
     disable = function(self)
-        if not self.config.gb_discards_sub and self.config.gb_hands_sub then
-            self.config.gb_discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
-            self.config.gb_hands_sub = math.floor(G.GAME.current_round.hands_left / 2) 
+        if not self.config.extra.discards_sub and self.config.extra.hands_sub then
+            self.config.extra.discards_sub = math.floor(G.GAME.current_round.discards_left / 2)
+            self.config.extra.hands_sub = math.floor(G.GAME.current_round.hands_left / 2) 
         end
-        ease_discard(self.config.gb_discards_sub)
-        ease_hands_played(self.config.gb_hands_sub)
+        ease_discard(self.config.extra.discards_sub)
+        ease_hands_played(self.config.extra.hands_sub)
     end
 }
