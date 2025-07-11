76afe27ae0bf4daead45c3d9cc80df492689bb1c
thefaketh30ne <thefaketh30ne@gmail.com>
2025-07-11 19:54:38 +0100

    3 new boss jokers

---
+++ modules/content/jokers/4 - boss/2 - grab bag/the_mind.lua
@@ -0,0 +1,42 @@
+SMODS.Joker{
+    key = "mind",
+    loc_txt = {
+		name = 'The Mind',
+		text = {
+			"When hand is played, {C:attention}flips{} all",
+            "{C:attention}face-up{} {C:hearts}Hearts{} and {C:diamonds}Diamonds{} held in hand",
+            "Cards flipped by this Joker",
+            "{C:attention}permanently{} gain {C:mult}+#1#{} Mult",
+		}
+	},
+    blueprint_compat = true,
+	atlas = 'gb_BossJokers',
+	pos = { x = 4, y = 2 },
+    rarity = "gb_boss",
+    cost = 6,
+    config = { extra = { mult = 6 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
+    end,
+
+    calculate = function(self, card, context)
+        if context.before then
+            for _, playing_card in ipairs(G.hand.cards) do
+                if (playing_card:is_suit("Hearts")
+                or playing_card:is_suit("Diamonds"))
+                and playing_card.facing == "front" then
+                    playing_card.ability.perma_mult = (playing_card.ability.perma_mult or 0) + card.ability.extra.mult
+                    playing_card:flip()
+                    SMODS.calculate_effect( {
+                        message = localize('k_upgrade_ex'),
+                        message_card = playing_card,
+                        colour = G.C.MULT
+                    }, card)
+                end
+            end
+        end
+    end,
+    in_pool = function(self, args)
+        return gb_is_blind_defeated("bl_gb_mind")
+    end
+}
---
+++ modules/content/jokers/4 - boss/2 - grab bag/the_piper.lua
@@ -0,0 +1,43 @@
+SMODS.Joker{
+    key = "piper",
+    loc_txt = {
+		name = 'The Piper',
+		text = {
+			"After hand scores, {C:green}#1# in #2# chance{} to",
+            "{C:attention}destroy{} the {C:attention}leftmost{} card and",
+            "give a {C:attention}third{} of its base {C:chips}Chips{} as {C:money}Money{}"
+		}
+	},
+    blueprint_compat = true,
+	atlas = 'gb_BossJokers',
+	pos = { x = 5, y = 2 },
+    rarity = "gb_boss",
+    cost = 6,
+    config = { extra = { odds = 2 } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = { G.GAME.probabilities.normal or 1, card.ability.extra.odds } }
+	end,
+    calculate = function(self, card, context)
+        if context.before 
+        and context.scoring_hand[1] 
+        and SMODS.pseudorandom_probability(card, 'gb_piper', G.GAME.probabilities.normal, card.ability.extra.odds) then
+            context.scoring_hand[1].ability.piper_destroy = true
+        end
+        if context.destroy_card and context.destroy_card.ability.piper_destroy == true then
+            if not SMODS.has_no_rank(context.destroy_card)
+            and context.destroy_card.base.nominal
+            and context.destroy_card.base.nominal > 0 then
+                return {
+                    remove = true,
+                    dollars = math.floor(context.destroy_card.base.nominal / 3)
+                }
+            end
+            return {
+                remove = true
+            }
+        end
+    end,
+    in_pool = function(self, args)
+        return gb_is_blind_defeated("bl_gb_piper")
+    end
+}
