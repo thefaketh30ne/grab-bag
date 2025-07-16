df6b8bafefe86dd64dd9a3f9d611547d4c35c358
thefaketh30ne <thefaketh30ne@gmail.com>
2025-07-16 23:44:48 +0100

    reorganise spectrals, shatter event fix

--- modules/content/consumables/spectral/shattered/shatter.lua
+++ modules/content/consumables/spectral/shattered/shatter.lua
@@ -20,19 +20,17 @@ SMODS.Consumable {
     can_use = function(self, card)
         return (#gb_find_eligible_shatters() > 0)
     end,
+
+    in_pool = function(self, args)
+        return (#gb_find_eligible_shatters() > 0)
+    end,
     
     use = function(self, card, area, copier)
         local outgoing_joker_key = pseudorandom_element(gb_find_eligible_shatters(), "gb_shatter")
         local outgoing_joker = SMODS.find_card(outgoing_joker_key)[1]
         -- Find the corresponding shattered joker
         local incoming_joker_key = GB_SHATTERED_TABLE[outgoing_joker_key]
-                -- Remove the original joker and add the shattered one
-        outgoing_joker:start_dissolve()
-        SMODS.add_card{
-            set = 'Joker',
-            key = incoming_joker_key,
-            edition = (outgoing_joker.edition and outgoing_joker.edition.key) or nil
-        }
+        -- Remove the original joker and add the shattered one
         G.E_MANAGER:add_event(Event({
         trigger = 'after',
         delay = 0.4,
@@ -48,12 +46,24 @@ SMODS.Consumable {
                 offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and -0.2 or 0 },
                 silent = true
             })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15 * G.SETTINGS.GAMESPEED,
+                blockable = false,
+                blocking = false,
+                func = function()
+                    outgoing_joker:start_dissolve()
+                    SMODS.add_card{
+                    set = 'Joker',
+                    key = incoming_joker_key,
+                    edition = (outgoing_joker.edition and outgoing_joker.edition.key) or nil
+                }
+                return true
+                end
+                    }))
             card:juice_up(0.3, 0.5)
             return true
             end
         }))
     end,
-    in_pool = function(self, args)
-        return (#gb_find_eligible_shatters() > 0)
-    end
 }
