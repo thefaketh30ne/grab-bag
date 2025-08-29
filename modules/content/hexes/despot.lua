7b1301fd4ab3f28e897af28a318ed6cd676c10b6
thefaketh30ne <thefaketh30ne@gmail.com>
2025-08-29 19:24:39 +0100

    fix rare despot crash

--- modules/content/hexes/despot.lua
+++ modules/content/hexes/despot.lua
@@ -15,12 +15,14 @@ GB.Hex {
     calculate = function(self, card, context)
         if context.before and context.cardarea == G.play then
             local card_to_randomise = pseudorandom_element(G.hand.cards, pseudoseed("gb_despot"))
-            assert(SMODS.change_base(
-                card_to_randomise,
-                pseudorandom_element(SMODS.Suits, pseudoseed("gb_despot")).key,
-                pseudorandom_element(SMODS.Ranks, pseudoseed("gb_despot")).key
-            ))
-            card_to_randomise:juice_up()
+            if card_to_randomise then
+                assert(SMODS.change_base(
+                    card_to_randomise,
+                    pseudorandom_element(SMODS.Suits, pseudoseed("gb_despot")).key,
+                    pseudorandom_element(SMODS.Ranks, pseudoseed("gb_despot")).key
+                ))
+                card_to_randomise:juice_up()
+            end
         end
     end
 }
