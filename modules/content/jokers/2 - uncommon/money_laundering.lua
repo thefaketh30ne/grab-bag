8f018ddabb262f3ef23c893a08d2e1d852cf5395
thefaketh30ne <thefaketh30ne@gmail.com>
2025-10-19 16:21:18 +0100

    add money laundering

---
+++ modules/content/jokers/2 - uncommon/money_laundering.lua
@@ -0,0 +1,16 @@
+SMODS.Joker {
+	key = 'money_laundering',
+	loc_txt = {
+		name = 'Money Laundering',
+		text = {
+			"{C:attention}Double{} all {C:money}Income",
+            "Can only earn up to",
+            "{C:money}$10{} at one time"
+		}
+	},
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 7, y = 5 },
+	cost = 6,
+	blueprint_compat = false,
+}
