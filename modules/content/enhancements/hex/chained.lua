1d37507f62a8f03ffb84c798fcf0f458e51f99a0
thefaketh30ne <thefaketh30ne@gmail.com>
2025-10-19 14:40:42 +0100

    Update chained.lua

--- modules/content/enhancements/hex/chained.lua
+++ modules/content/enhancements/hex/chained.lua
@@ -4,22 +4,18 @@ SMODS.Enhancement {
 		name = 'Chained Card',
 		text = {
 			"{C:chips}+#1#{} Chips for every",
-            "{C:attention}Chained{} card in {C:attention}full deck"
+            "{C:attention}Chained{} card in {C:attention}full deck",
+            "{C:inactive}(Currently{C:chips}+#1#{C:inactive} Chips)"
 		}
 	},
     atlas = 'gb_Enhancements',
     pos = { x = 1, y = 0 },
     config = { bonus = 0, extra = { chips_mod = 20 } },
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.chips_mod } }
-    end,
-    set_ability = function(self, card, initial, delay_sprites)
-        if G.deck then
-            card.ability.bonus = card.ability.extra.chips_mod * gb_tally_enhancements(G.playing_cards, "m_gb_chained")
-        end
+        return { vars = { card.ability.extra.chips_mod, card.ability.extra.chips_mod * gb_tally_enhancements(G.playing_cards, "m_gb_chained") } }
     end,
     calculate = function(self, card, context)
-        if context.before then
+        if context.main_scoring and context.cardarea == G.play then
             card.ability.bonus = card.ability.extra.chips_mod * gb_tally_enhancements(G.playing_cards, "m_gb_chained")
         end
     end,
