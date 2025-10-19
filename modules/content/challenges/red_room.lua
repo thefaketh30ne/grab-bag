6e680e6f9d5ed69f418ca71eec84119c2ec119ae
thefaketh30ne <thefaketh30ne@gmail.com>
2025-10-19 20:49:32 +0100

    challenges! reworked Hit the Slots! and added Red Room

---
+++ modules/content/challenges/red_room.lua
@@ -0,0 +1,15 @@
+SMODS.Challenge {
+    key = 'red_room',
+    loc_txt = {
+        name = "Red Room"
+    },
+    jokers = {
+        { id = 'j_oops',  eternal = true, edition = 'negative' },
+        { id = 'j_oops',  eternal = true, edition = 'negative' },
+        { id = 'j_oops',  eternal = true, edition = 'negative' },
+    },
+    deck = {
+        type = 'Challenge Deck',
+        enhancement = 'm_gb_macabre'
+    }
+}
