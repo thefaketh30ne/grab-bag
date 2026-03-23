74f0097ba3ee3b6a6524ec8678a861b9105270cf
Sophie <38859940+Yule42@users.noreply.github.com>
2026-03-22 20:20:19 -0700

    Rewrite Pike

--- modules/content/blinds/2 - showdown/final_pike.lua
+++ modules/content/blinds/2 - showdown/final_pike.lua
@@ -14,23 +14,81 @@ SMODS.Blind {
     pos = { y = 16 },
     boss = { showdown = true },
     boss_colour = HEX("a9b4b9"),
+    config = { extra = { pause_triggering = false } },
     calculate = function(self, blind, context)
         if not blind.disabled then
-            if (context.pre_discard or context.before) and not context.hook then
-                local selection_limit = G.hand.config.highlighted_limit
-                G.hand.config.highlighted_limit = 1000
-                local any_selected = false
-                for _, playing_card in pairs(G.hand.cards) do
-                    if not playing_card.highlighted then
-                        G.hand:add_to_highlighted(playing_card, true)
-                        any_selected = true
+            if context.press_play then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        local any_selected = nil
+                        local selection_limit = G.hand.config.highlighted_limit
+                        G.hand.config.highlighted_limit = 10000
+                        for _, playing_card in pairs(G.hand.cards) do
+                            G.hand:add_to_highlighted(playing_card, true)
+                            any_selected = true
+                        end
+                        if any_selected then G.FUNCS.discard_cards_from_highlighted(nil, true) end
+                        G.hand.config.highlighted_limit = selection_limit
+                        return true
                     end
-                end
-                if any_selected then 
-                    G.FUNCS.discard_cards_from_highlighted(nil, true) 
-                end
-                G.hand.config.highlighted_limit = selection_limit
+                }))
                 blind.triggered = true
+                delay(0.7)
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'immediate',
+                    func = (function()
+                        SMODS.juice_up_blind()
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.06 * G.SETTINGS.GAMESPEED,
+                            blockable = false,
+                            blocking = false,
+                            func = function()
+                                play_sound('tarot2', 0.76, 0.4); return true
+                            end
+                        }))
+                        play_sound('tarot2', 1, 0.4)
+                        return true
+                    end)
+                }))
+                delay(0.4)
+            elseif context.gb_after_discard and G.GAME.blind.effect.extra and not G.GAME.blind.effect.extra.pause_triggering then
+                G.GAME.blind.effect.extra.pause_triggering = true -- don't want this discard to trigger it again
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        local any_selected = nil
+                        local selection_limit = G.hand.config.highlighted_limit
+                        G.hand.config.highlighted_limit = 10000
+                        for _, playing_card in pairs(G.hand.cards) do
+                            G.hand:add_to_highlighted(playing_card, true)
+                            any_selected = true
+                        end
+                        if any_selected then G.FUNCS.discard_cards_from_highlighted(nil, true) end
+                        G.hand.config.highlighted_limit = selection_limit
+                        G.GAME.blind.effect.extra.pause_triggering = false
+                        return true
+                    end
+                }))
+                blind.triggered = true
+                delay(0.7)
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'immediate',
+                    func = (function()
+                        SMODS.juice_up_blind()
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.06 * G.SETTINGS.GAMESPEED,
+                            blockable = false,
+                            blocking = false,
+                            func = function()
+                                play_sound('tarot2', 0.76, 0.4); return true
+                            end
+                        }))
+                        play_sound('tarot2', 1, 0.4)
+                        return true
+                    end)
+                }))
+                delay(0.4)
             end
         end
     end
