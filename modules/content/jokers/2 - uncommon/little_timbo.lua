4d68c07f59c6354415c1931da32ff98b7c5fe746
thefaketh30ne <thefaketh30ne@gmail.com>
2026-08-28 20:10:39 +0100

    rotten cards and timbo now use xblindsize

--- modules/content/jokers/2 - uncommon/little_timbo.lua
+++ modules/content/jokers/2 - uncommon/little_timbo.lua
@@ -7,21 +7,20 @@ SMODS.Joker{
             "{C:red}#2#{} Joker slot",
 		}
     },
-    config = { extra = { blind_size = 0.5, joker_slot = -1 } },
+    config = { extra = { xblind_size = 0.5, joker_slot = -1 } },
 	rarity = 2,
 	atlas = 'gb_Jokers',
 	pos = { x = 2, y = 0 },
 	cost = 6,
 	blueprint_compat = false,
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.blind_size, card.ability.extra.joker_slot } }
+        return { vars = { card.ability.extra.x_blind_size, card.ability.extra.joker_slot } }
     end,
     calculate = function(self, card, context)
         if context.setting_blind then
-            G.GAME.blind.chips = G.GAME.blind.chips * card.ability.extra.blind_size
-            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
-            play_sound("cancel")
-            card:juice_up()
+            return {
+                xblindsize = card.ability.extra.x_blind_size
+            }
         end
     end,
     add_to_deck = function(self, card, from_debuff)
