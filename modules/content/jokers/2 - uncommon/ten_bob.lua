cc948781c3c8bd9f40e1a1ffbd83616165da6391
thefaketh30ne <thefaketh30ne@gmail.com>
2026-08-31 03:20:35 +0100

    fix ten bob

--- modules/content/jokers/2 - uncommon/ten_bob.lua
+++ modules/content/jokers/2 - uncommon/ten_bob.lua
@@ -20,7 +20,6 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.end_of_round
-        and context.main_eval
         and not context.blueprint then
             local valid_targets = {}
             for _, joker in pairs(G.jokers.cards) do
