0c9357cb1eeb69c07c3b149783a4985171bfee9f
thefaketh30ne <thefaketh30ne@gmail.com>
2026-09-02 16:55:26 +0100

    floppy disk now uses SMODS.copy_card

--- modules/content/jokers/2 - uncommon/floppy_disk.lua
+++ modules/content/jokers/2 - uncommon/floppy_disk.lua
@@ -32,27 +32,11 @@ SMODS.Joker {
         if context.selling_self and not context.blueprint then
             local copied_cards = {}
             for _, saved_card in ipairs(card.ability.extra.saved_cards) do
-                G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                local copy_card = copy_card(saved_card, nil, nil, G.playing_card)
-                copy_card:add_to_deck()
-                G.deck.config.card_limit = G.deck.config.card_limit + 1
-                table.insert(G.playing_cards, copy_card)
-                G.deck:emplace(copy_card)
-                table.insert(copied_cards, copy_card)
+                SMODS.copy_card(saved_card, nil, nil, nil, nil, G.deck)
             end
             return {
                 message = "Copied " .. #copied_cards .. " Cards!",
-                colour = G.C.FILTER,
-                func = function()
-                    G.E_MANAGER:add_event(Event({
-                        func = function()
-                            if #copied_cards > 0 then
-                                SMODS.calculate_context({ playing_card_added = true, cards = copied_cards })
-                            end
-                            return true
-                        end
-                    }))
-                end
+                colour = G.C.FILTER
             }
         end
     end
